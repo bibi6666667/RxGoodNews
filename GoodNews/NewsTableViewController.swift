04db7566d24401d463d11a85dbf68886e8806f42
Bibi <bibi6666667@kakao.com>
2022-12-27 11:10:04 +0900

    55. Fetching News from NewsAPI

--- GoodNews/NewsTableViewController.swift
+++ GoodNews/NewsTableViewController.swift
@@ -15,5 +15,27 @@ class NewsTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationController?.navigationBar.prefersLargeTitles = true
+        
+        populateNews()
+    }
+    
+    private func populateNews() {
+        let url = URL(string: "https://newsapi.org/v2/top-headlines?country=us&apiKey=API키")!
+        
+        Observable.just(url)
+            .flatMap { url -> Observable<Data> in
+                                            // 리턴타입 명시한 이유 : 타입 추론이 안 될 수 있기 때문. 특히 지금처럼 여러 결과를 join하는 경우에.
+                let request = URLRequest(url: url)
+                return URLSession.shared.rx.data(request: request) // Observable<data> 반환
+            }.map { data -> [Article]? in
+                return try? JSONDecoder().decode(ArticleList.self, from: data).articles
+            }.subscribe(onNext: { [weak self] articles in
+                if let articles = articles {
+                    DispatchQueue.main.async {
+                        self?.tableView.reloadData()
+                    }
+                }
+                
+            })
     }
 }

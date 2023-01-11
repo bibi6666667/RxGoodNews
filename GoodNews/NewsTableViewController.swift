fb9b94ae830106c8641a405ecc7bc2f1196cda9e
Bibi <bibi6666667@kakao.com>
2023-01-11 21:50:51 +0900

    58. Implementing URLRequest Observable to Perform Request

--- GoodNews/NewsTableViewController.swift
+++ GoodNews/NewsTableViewController.swift
@@ -23,7 +23,6 @@ class NewsTableViewController: UITableViewController {
         populateNews()
     }
     
-<<<<<<< HEAD
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
@@ -39,38 +38,29 @@ class NewsTableViewController: UITableViewController {
         
         cell.titleLabel.text = self.articles[indexPath.row].title
         cell.descriptionLabel.text = self.articles[indexPath.row].description
-       
+        
         return cell
     }
     
+    
     private func populateNews() {
-        
-        URLRequest.load(resource: ArticleList.all)
-            .subscribe(onNext: { [weak self] articleList in
-                if let articleList = articleList {
-                    self?.articles = articleList.articles
-=======
-    private func populateNews() {
-        let url = URL(string: "https://newsapi.org/v2/top-headlines?country=us&apiKey=e2bc329d2de34c4f94cbe2e9dcfc6dff")!
+        let url = URL(string: "https://newsapi.org/v2/top-headlines?country=kr&apiKey=API키")!
         
         Observable.just(url)
-            .flatMap { url -> Observable<Data> in // 리턴타입 명시한 이유 : 타입 추론이 안 될 수 있기 때문. 특히 지금처럼 여러 결과를 join하는 경우에.
+            .flatMap { url -> Observable<Data> in
+                // 리턴타입 명시한 이유 : 타입 추론이 안 될 수 있기 때문. 특히 지금처럼 여러 결과를 join하는 경우에.
                 let request = URLRequest(url: url)
                 return URLSession.shared.rx.data(request: request) // Observable<data> 반환
             }.map { data -> [Article]? in
                 return try? JSONDecoder().decode(ArticleList.self, from: data).articles
             }.subscribe(onNext: { [weak self] articles in
                 if let articles = articles {
->>>>>>> 9b972556f54a163b19f9e23478e0d390d0724fa8
+                    self?.articles = articles
                     DispatchQueue.main.async {
                         self?.tableView.reloadData()
                     }
                 }
-<<<<<<< HEAD
             }).disposed(by: disposeBag)
-=======
-                
-            })
->>>>>>> 9b972556f54a163b19f9e23478e0d390d0724fa8
     }
+    
 }
